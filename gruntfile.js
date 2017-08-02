153e1e7df0be975e0d70f72d570ddd55895b2f4a
Najim Mazidi <nmazidi95@gmail.com>
2017-08-03 00:35:42 +0100

    Fixed problem with LESS task runner - was compiling to wrong css file

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: {
-          "css/spongetest.css": "app/less/spongetest.less"
+          "app/css/spongetest.css": "app/less/spongetest.less"
         }
       }
     },
