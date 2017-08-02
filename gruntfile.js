63f25308b604ac8f4508b64c556575e343b44bd4
Najim Mazidi <nmazidi95@gmail.com>
2017-08-02 22:55:05 +0100

    Task 10: Grunt LESS task runner installed

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "css/spongetest.css": "app/less/spongetest.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['app/less/**/*.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
